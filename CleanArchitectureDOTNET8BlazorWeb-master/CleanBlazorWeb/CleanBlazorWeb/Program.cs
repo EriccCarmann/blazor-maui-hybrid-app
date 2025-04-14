cb5f71054287ab50c52260c47cf99c0eb15c793e
Sasha Manyonok <83176347+EriccCarmann@users.noreply.github.com>
2025-04-14 14:16:06 +0300

    added shared components

--- CleanArchitectureDOTNET8BlazorWeb-master/CleanBlazorWeb/CleanBlazorWeb/Program.cs
+++ CleanArchitectureDOTNET8BlazorWeb-master/CleanBlazorWeb/CleanBlazorWeb/Program.cs
@@ -66,8 +66,8 @@ namespace CleanBlazorWeb
             app.MapRazorComponents<App>()
                 .AddInteractiveServerRenderMode()
                 .AddInteractiveWebAssemblyRenderMode()
-                .AddAdditionalAssemblies(typeof(Client._Imports).Assembly,
-                                         typeof(RazorMauiShared._Imports).Assembly);
+                .AddAdditionalAssemblies(typeof(RazorMauiShared._Imports).Assembly,
+                                         typeof(Client._Imports).Assembly);
 
             // Add additional endpoints required by the Identity /Account Razor components.
             app.MapAdditionalIdentityEndpoints();
